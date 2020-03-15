1b5445fe9eb8691ddd1e14501c16f77ee9aa8d73
pjqdyd <1909025079@qq.com>
2020-03-15 16:47:15 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 #### SpringBoot整合Sharding-jdbc实现分库分表解决方案的案例
 
-1. 准备两个不同的数据源: db_myshop_0, db_myshop_1
+1. 准备两个不同的数据源: 
+    如:
+    
+    jdbc:mysql://localhost:3306/myshop_0db_myshop_0
+    
+    jdbc:mysql://localhost:33060/myshop_0db_myshop_1
 
-2. 分别在两个库中运行sharding-jdbc.sql建表文件
+2. 分别在两个库中运行sharding  -jdbc.sql建表文件
---
+++ src/main/java/com/pjqdyd/SpringBootShardingJDBCApplication.java
@@ -0,0 +1,18 @@
+package com.pjqdyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**   
+ * @Description:  [启动主类]
+ * @Author:       pjqdyd
+ * @Version:      [v1.0.0]
+ */
+
+@SpringBootApplication
+public class SpringBootShardingJDBCApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootShardingJDBCApplication.class);
+    }
+}
